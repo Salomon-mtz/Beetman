32de68423f4f490592ca02e56469af4448e78510
salomon <a01653876@tec.mx>
2022-11-11 07:20:46 -0600

    Urls and settings update

--- beetmann/beetmann/settings.py
+++ beetmann/beetmann/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = 'django-insecure-dq%%qlsczkt)u6tl@j_$#2db!d+3o^)ne!bfda15k)kgned-1w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'alux',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
